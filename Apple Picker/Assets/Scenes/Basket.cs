b4044345b91a16c13610127da44082084cb6458d
Ariana K <arianackim@gmail.com>
2023-02-08 16:44:17 -0600

    Added GUI & Game Management
    Addition of GUI elements (score, high score counters) and game management elements (lives, game over) to the game

--- Apple Picker/Assets/Scenes/Basket.cs
+++ Apple Picker/Assets/Scenes/Basket.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
+    public ScoreCounter scoreCounter;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Find a GameObject named ScoreCounter in the Scene Hierarchy
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the ScoreCounter (Script) component of scoreGO
+        scoreCounter = scoreGO.GetComponent<ScoreCounter>();
         
     }
 
@@ -33,6 +39,9 @@ public class Basket : MonoBehaviour
         GameObject collidedWith = coll.gameObject;
         if(collidedWith.CompareTag("Apple")) {
             Destroy(collidedWith);
+            // Increase the score
+            scoreCounter.score += 100;
+            HighScore.TRY_SET_HIGH_SCORE(scoreCounter.score);
         }
     }
 }
